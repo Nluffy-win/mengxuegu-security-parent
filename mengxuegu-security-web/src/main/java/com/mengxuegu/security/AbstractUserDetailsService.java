54c03c0cc389b7109307060b0eae47040fc689c8
赵杰 <7806764+nluffy@user.noreply.gitee.com>
2020-10-12 11:23:15 +0800

    抽取公共代码块

---
+++ mengxuegu-security-web/src/main/java/com/mengxuegu/security/AbstractUserDetailsService.java
@@ -0,0 +1,70 @@
+package com.mengxuegu.security;
+
+import com.mengxuegu.web.entities.SysPermission;
+import com.mengxuegu.web.entities.SysUser;
+import com.mengxuegu.web.service.SysPermissionService;
+import org.apache.commons.collections.CollectionUtils;
+import org.assertj.core.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+/**
+ * 根据用户名和手机号查询的公共方法
+ * Created by Y_Coffee on 2020/10/12
+ * @author CoffeeY
+ */
+public abstract class AbstractUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private SysPermissionService sysPermissionService;
+
+    /**
+     * 将方法交给子类去查询用户名密码，手机号
+     * @param usernameOrMobile
+     * @return
+     */
+    public abstract SysUser findSysUser(String usernameOrMobile);
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrMobile) throws UsernameNotFoundException {
+
+        // 1. 通过请求的用户名去数据库中查询用户信息
+        SysUser sysUser = findSysUser(usernameOrMobile);
+        //引用
+        findSysPermission(sysUser);
+        return sysUser;
+    }
+
+    private void findSysPermission(SysUser sysUser){
+        if (sysUser == null){
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+        // 2. 查询该用户有哪一些权限
+        List<SysPermission> permissions = sysPermissionService.findByID(sysUser.getId());
+        if (CollectionUtils.isNotEmpty(permissions)){
+            return ;
+        }
+
+        // 在左侧菜单 动态渲染会使用，目前先把它都传入
+        sysUser.setPermissions(permissions);
+
+        // 3. 封装用户信息和权限信息
+        //用户信息sysuser自动封装
+        //封装权限
+        List<GrantedAuthority> authorities = Lists.newArrayList();
+        for (SysPermission sp : permissions){
+            String code = sp.getCode();
+            //因为code是String类型，需要转换才能存入
+            authorities.add(new SimpleGrantedAuthority(code));
+        }
+        sysUser.setAuthorities(authorities);
+
+        // 4. 交给springsecurity自动进行身份认证
+    }
+}
--- mengxuegu-security-web/src/main/java/com/mengxuegu/security/MobileUserDetailsService.java
+++ mengxuegu-security-web/src/main/java/com/mengxuegu/security/MobileUserDetailsService.java
@@ -1,19 +1,9 @@
 package com.mengxuegu.security;
 
-import com.mengxuegu.web.entities.SysPermission;
 import com.mengxuegu.web.entities.SysUser;
-import com.mengxuegu.web.service.SysPermissionService;
 import com.mengxuegu.web.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.util.Lists;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * 手机号查询用户的信息
@@ -23,47 +13,20 @@ import java.util.List;
  */
 @Slf4j
 @Component("mobileUserDetailsService")
-public class MobileUserDetailsService implements UserDetailsService {
+public class MobileUserDetailsService extends AbstractUserDetailsService {
 
     private final SysUserService sysUserService;
-    private final SysPermissionService sysPermissionService;
 
-    public MobileUserDetailsService(SysUserService sysUserService, SysPermissionService sysPermissionService) {
+    public MobileUserDetailsService(SysUserService sysUserService) {
         this.sysUserService = sysUserService;
-        this.sysPermissionService = sysPermissionService;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String mobile) throws UsernameNotFoundException {
-
-        log.info("验证的手机号：" + mobile);
+    public SysUser findSysUser(String usernameOrMobile) {
+        log.info("验证的手机号：" + usernameOrMobile);
         //通过手机查询用户信息
-        SysUser users = sysUserService.findByMobile(mobile);
-        if (users == null) {
-            throw new UsernameNotFoundException("手机号码尚未注册");
-        }
-        //如有此用户，查询对应权限
-        List<SysPermission> permissions = sysPermissionService.findByID(users.getId());
-
-        if (permissions == null) {
-            return users;
-        }
-
-        // 在左侧菜单 动态渲染会使用，目前先把它都传入
-        users.setPermissions(permissions);
-
-        //封装用户信息
-        List<GrantedAuthority> authorities = Lists.newArrayList();
-        for (SysPermission sp : permissions) {
-            String code = sp.getCode();
-            //因为code是String类型，需要转换才能存入
-            authorities.add(new SimpleGrantedAuthority(code));
-        }
-
-        users.setAuthorities(authorities);
-//        return new User("zhaojie", "", true, true, true, true,
-//                AuthorityUtils.commaSeparatedStringToAuthorityList("ADMIN"));
-
+        SysUser users = sysUserService.findByMobile(usernameOrMobile);
         return users;
     }
+
 }
